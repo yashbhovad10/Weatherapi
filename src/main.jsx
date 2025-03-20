4feba230960f26e2c3d6a380be79250687884adc
yashbhovad10 <yashbhovad9@gmail.com>
2025-03-20 11:19:20 +0530

    Updates

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './Fivedaysforcast.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <App />
-)
--- src/show.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const show = () => {
-  return <></>;
-  
-  
-};
-export default show;
